f2d5c8d3bfb5f80d760b9d3d17521daefdb071b4
tommyrac <155997776+tommyrac@users.noreply.github.com>
2025-04-10 23:43:24 -0600

    Update main.py
    fix password hash method

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def register():
             return jsonify({'message': 'Username already exists!'}), 409
 
         # Hash the password
-        hashed_password = generate_password_hash(password, method='sha256')
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
 
         # Create new user
         create_user(username, hashed_password, is_admin)
